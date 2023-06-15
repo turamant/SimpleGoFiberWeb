b35cacf11a46e3adcdb75aea3ee52dc65e25587f
askvart <tur1amant@gmail.com>
2023-06-15 13:17:20 +0300

    GET, POST /:event

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -10,7 +10,10 @@ import (
 )
 
 const profileUnknown = "unknown"
+const requestParamkeyEvent = "event"
+
 var counter int64 = 0
+var counters = make(map[string]int64)
 
 func main()  {
 	webApp := fiber.New()
@@ -20,6 +23,28 @@ func main()  {
 	})
 	webApp.Post("/counter", func(c *fiber.Ctx) error {
 		counter++
+		return c.SendStatus(http.StatusOK)
+	})
+
+	webApp.Get("/counter/:event", func(c *fiber.Ctx) error {
+		event := c.Params(requestParamkeyEvent, "")
+		if event == "" {
+			return c.SendStatus(http.StatusUnprocessableEntity)
+		}
+		eventCounter, ok := counters[event]
+		if !ok {
+			return c.SendStatus(http.StatusNotFound)
+		}
+		return c.SendString(strconv.FormatInt(eventCounter, 10))
+	})
+
+	webApp.Post("/counter/:event", func(c *fiber.Ctx) error {
+		event := c.Params(requestParamkeyEvent, "")
+		if event == "" {
+			return c.SendStatus(http.StatusUnprocessableEntity)
+		}
+		counters[event] += 1
+		
 		return c.SendStatus(http.StatusOK)
 	})
 
