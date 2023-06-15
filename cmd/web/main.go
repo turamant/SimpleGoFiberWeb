ec1e1f189ab071c494ecc6a37b6b4d30d8977b49
askvart <tur1amant@gmail.com>
2023-06-15 14:02:43 +0300

    json

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,6 +6,7 @@ import (
 	"strconv"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
 	"github.com/sirupsen/logrus"
 )
 
@@ -15,8 +16,47 @@ const requestParamkeyEvent = "event"
 var counter int64 = 0
 var counters = make(map[string]int64)
 
+type (
+	CreateLogEntryRequest struct{
+		Message string 	`json: "message"`
+		Level string   	`json: "level`
+		Timestamp int64	`json: "timestamp`
+	}
+
+	CreateLogEntryResponse struct {
+		ID string  	`json: "id"`
+	}
+
+	LogEntry struct {
+		ID 		  string
+		Message   string
+		Level     string
+		Timestamp int64
+	}
+)
+
+var logs []LogEntry
+
 func main()  {
 	webApp := fiber.New()
+
+	webApp.Post("/logs", func(c *fiber.Ctx) error {
+		var request CreateLogEntryRequest
+		if err := c.BodyParser(&request); err != nil {
+			return fmt.Errorf("body parser: %w", err)
+		}
+		logEntry := LogEntry{
+			ID: uuid.New().String(),
+			Message: request.Message,
+			Level: request.Level,
+			Timestamp: request.Timestamp,
+		}
+		logs = append(logs, logEntry)
+		return c.JSON(CreateLogEntryResponse{
+			ID: logEntry.ID,
+		})
+	})
+
 
 	webApp.Get("/counter", func(c *fiber.Ctx) error {
 		return c.SendString(strconv.FormatInt(counter, 10))
