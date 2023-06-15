3569a2440fd0e0501b7f609ea28b069f6ea5dfae
askvart <tur1amant@gmail.com>
2023-06-15 13:04:10 +0300

    GET, POST /counter

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,15 +3,33 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/sirupsen/logrus"
 )
 
 const profileUnknown = "unknown"
+var counter int64 = 0
 
 func main()  {
 	webApp := fiber.New()
+
+	webApp.Get("/counter", func(c *fiber.Ctx) error {
+		return c.SendString(strconv.FormatInt(counter, 10))
+	})
+	webApp.Post("/counter", func(c *fiber.Ctx) error {
+		counter++
+		return c.SendStatus(http.StatusOK)
+	})
+
+	webApp.Get("/about", func(c *fiber.Ctx) error {
+		return c.SendString("The best school for Software Engineers")
+	})
+
+	webApp.Get("/courses", func(c *fiber.Ctx) error {
+		return c.SendString("Java, Python, Go")
+	})
 
 	webApp.Get("/address" , func(c *fiber.Ctx) error {
 		return c.SendString("Moscow, Red square 11a")
