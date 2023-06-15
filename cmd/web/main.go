4bf51c1f037103b211d7565dcc0d795c0b531b5e
askvart <tur1amant@gmail.com>
2023-06-15 12:47:40 +0300

    /profiles

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,33 +1,35 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/gofiber/fiber/v2"
 	"github.com/sirupsen/logrus"
 )
 
+const profileUnknown = "unknown"
+
 func main()  {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		_, err := w.Write([]byte("hello ome page"))
-		if err != nil {
-			logrus.WithError(err).Error("write hello home oage")
-		}
-	})
-	mux.HandleFunc("/course", func(w http.ResponseWriter, r *http.Request){
-		page := r.URL.Query().Get("page")
-		_, err := w.Write([]byte(page))
-		if err != nil {
-			logrus.WithError(err).Error("Ошибка печати")
-		}
+	webApp := fiber.New()
+
+	webApp.Get("/address" , func(c *fiber.Ctx) error {
+		return c.SendString("Moscow, Red square 11a")
 	})
 
-    port := "4000"
-	logrus.WithFields(logrus.Fields{
-		"port": port,
-	}).Info("Starting a web-serwer on port")
+    webApp.Get("/profiles", func(c *fiber.Ctx) error {
+		profileID := c.Query("profile_id", profileUnknown)
+		if profileID == "" {
+			profileID = profileUnknown
+		}
+		if profileID == profileUnknown {
+			return c.Status(http.StatusUnprocessableEntity).SendString("profile_id is required")
+		}
+		return c.SendString(fmt.Sprintf("user Profile ID: %s", profileID))
+	})
 
-	logrus.Fatal(http.ListenAndServe(":" + port, mux))
+	port := "4000"
+	logrus.Fatal(webApp.Listen(":" + port))
 	
 	
 }
